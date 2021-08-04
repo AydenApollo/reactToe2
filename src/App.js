aeb4cd95c36aed7b200068b080b498265c36033f
aydenapollo <mcdonaldshane33@gmail.com>
2021-08-04 15:25:32 -0700

    tic tac toe revision

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Board from './board'
+import Game from './game'
 
-function App() {
+function Square(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
   );
 }
+<div>
+<Board/>
+<Game/>
+</div>
 
-export default App;
+
+export default Square
